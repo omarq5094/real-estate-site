4bbf77feebf08f57233f0598275b072278ae2d9e
omarq5094 <202308430@stu.jazanu.edu.sa>
2026-06-17 13:15:04 +0300

    Delete OFFICE-015_complete_site directory

--- OFFICE-015_complete_site/config.js
+++
@@ -1,6 +0,0 @@
-// Real Estate Platform Config
-const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxgUz6LOnOcFwT9sO4XCMwq6O7JEtEnoDH5RrzUoCsRvs-MOm3sILATY5vyvphn95Zw/exec",
-  DEFAULT_ADMIN_TOKEN: "",
-  MAX_IMAGES_PER_PROPERTY: 10
-};
